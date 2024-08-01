d79023e0c52b19698ac6cafc2457b7c9c79e2018
musebe <emusebe9@gmail.com>
2024-08-01 12:03:54 +0300

    aligned buttons

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -7,8 +7,8 @@ const Cta = () => {
   return (
     <section className='py-12 md:py-24 bg-tertiary dark:bg-secondary/40'>
       <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
-        <div className='flex flex-col-reverse md:flex-row items-center justify-between text-center md:text-left'>
-          <div className='md:w-1/2 mt-8 md:mt-0'>
+        <div className='flex flex-col-reverse md:flex-row items-center justify-between'>
+          <div className='md:w-1/2 mt-8 md:mt-0 text-left'>
             <h2 className='text-3xl sm:text-4xl lg:text-5xl mb-4 text-foreground'>
               Want to contribute to a better future? Join us in empowering
               communities through education.
@@ -17,11 +17,13 @@ const Cta = () => {
               Your involvement can make a significant difference in creating
               educational opportunities and building a brighter future for many.
             </p>
-            <Link href='/involved' passHref>
-              <Button className='py-2 px-4 text-sm sm:py-3 sm:px-6 sm:text-base gap-x-2 mt-4 flex items-center'>
-                Get Involved <ArrowRightCircle size={20} className='ml-2' />
-              </Button>
-            </Link>
+            <div className='flex justify-center md:justify-start'>
+              <Link href='/involved' passHref>
+                <Button className='py-2 px-4 text-sm sm:py-3 sm:px-6 sm:text-base gap-x-2 mt-4 flex items-center'>
+                  Get Involved <ArrowRightCircle size={20} className='ml-2' />
+                </Button>
+              </Link>
+            </div>
           </div>
           <div className='md:w-1/2 mb-8 md:mb-0'>
             <Image
