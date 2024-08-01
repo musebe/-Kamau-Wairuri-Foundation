0b0f780146b905c304eee9fd2a51949c4cba56f4
musebe <emusebe9@gmail.com>
2024-08-01 11:52:07 +0300

    aligned hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -10,11 +10,11 @@ const Hero = () => {
       <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
         <div className='flex flex-col md:flex-row justify-between gap-x-8 items-center'>
           {/* text */}
-          <div className='flex max-w-lg md:max-w-xl flex-col justify-center mx-auto md:mx-0 text-center md:text-left'>
+          <div className='flex max-w-lg md:max-w-xl flex-col justify-center mx-auto md:mx-0'>
             {/* socials and image on smaller screens */}
-            <div className='md:hidden flex flex-col items-center mb-4'>
+            <div className='md:hidden flex flex-col items-start mb-4'>
               <Socials
-                containerStyles='flex gap-x-6 justify-center mb-4'
+                containerStyles='flex gap-x-6 justify-start mb-4'
                 iconsStyles='text-foreground text-[22px] hover:text-primary transition-all'
               />
               <Image
@@ -29,16 +29,16 @@ const Hero = () => {
             <h1 className='text-4xl sm:text-5xl lg:text-6xl mb-4'>
               Kamau Wairuri Foundation
             </h1>
-            <p className='text-base sm:text-lg lg:text-xl subtitle max-w-lg mx-auto md:mx-0'>
+            <p className='text-base sm:text-lg lg:text-xl subtitle max-w-lg'>
               Kamau Wairuri Foundation exists to transform communities in Kenya
               through education. Inspired by the journey of our Founder and
               Patron, Dr. Kamau Wairuri, we are committed to providing support
               and opportunities for children and adults to access education that
-              enables them to fulfil their potential and contribute positively
+              enables them to fulfill their potential and contribute positively
               to community development.
             </p>
             {/* buttons */}
-            <div className='flex flex-row gap-y-3 gap-x-3 justify-center md:justify-start mb-12'>
+            <div className='flex flex-row gap-y-3 gap-x-3 justify-start mb-12'>
               <Link href='/contact'>
                 <Button className='py-2 px-4 text-sm sm:py-3 sm:px-6 sm:text-base gap-x-2'>
                   Contact us <Send size={18} />
@@ -55,9 +55,9 @@ const Hero = () => {
             </div>
 
             {/* socials on medium and larger screens */}
-            <div className='hidden md:flex gap-x-6 justify-start'>
+            <div className='hidden md:flex gap-x-6'>
               <Socials
-                containerStyles='flex gap-x-6 justify-start'
+                containerStyles='flex gap-x-6'
                 iconsStyles='text-foreground text-[22px] hover:text-primary transition-all'
               />
             </div>
