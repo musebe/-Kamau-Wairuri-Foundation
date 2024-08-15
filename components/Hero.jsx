24846b8f0bea9a4e2efcc07d575e4932dfde3c77
Kelvin Macharia Ngunyi <ngunyimacharia@gmail.com>
2024-08-15 11:04:19 +0300

    fix: improve Hero responsiveness

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,35 +1,31 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { Button } from './ui/button';
-import { HeartHandshake, Send } from 'lucide-react';
-import Socials from './Socials';
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { HeartHandshake, Send } from "lucide-react";
+import Socials from "./Socials";
 
 const Hero = () => {
   return (
-    <section className='py-12 md:py-24 h-auto md:h-[84vh] md:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none'>
-      <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
-        <div className='flex flex-col md:flex-row justify-between gap-x-8 items-center'>
+    <section className="py-12 md:py-24 h-auto md:h-[84vh] md:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col lg:flex-row justify-between gap-x-8 items-center">
           {/* text */}
-          <div className='flex max-w-lg md:max-w-xl flex-col justify-center mx-auto md:mx-0'>
+          <div className="flex max-w-full lg:max-w-xl flex-col justify-center lg:mx-0">
             {/* socials and image on smaller screens */}
-            <div className='md:hidden flex flex-col items-center mb-4'>
-              <Socials
-                containerStyles='flex gap-x-6 justify-center mb-4'
-                iconsStyles='text-foreground text-[22px] hover:text-primary transition-all'
-              />
+            <div className="lg:hidden flex flex-col items-center mb-4">
               <Image
-                src='/hero/home.JPG'
-                alt='Kamau Wairuri Foundation'
-                width={350}
-                height={318}
-                className='rounded-xl shadow-lg'
-                style={{ width: 'auto', height: 'auto' }}
+                src="/hero/home.JPG"
+                alt="Kamau Wairuri Foundation"
+                width={1000}
+                height={455}
+                className="rounded-xl shadow-lg mx-auto"
+                style={{ width: "auto", height: "auto" }}
               />
             </div>
-            <h1 className='text-4xl sm:text-5xl lg:text-6xl mb-4 text-center md:text-left'>
+            <h1 className="text-4xl sm:text-5xl lg:text-6xl my-8 text-center lg:text-left">
               Kamau Wairuri Foundation
             </h1>
-            <p className='text-base sm:text-lg lg:text-xl subtitle max-w-lg mx-auto md:mx-0 text-left'>
+            <p className="text-base sm:text-lg lg:text-xl subtitle mx-auto lg:mx-0 text-left">
               Kamau Wairuri Foundation exists to transform communities in Kenya
               through education. Inspired by the journey of our Founder and
               Patron, Dr. Kamau Wairuri, we are committed to providing support
@@ -38,9 +34,9 @@ const Hero = () => {
               to community development.
             </p>
             {/* buttons */}
-            <div className='flex flex-row gap-y-3 gap-x-3 justify-start mb-12'>
-              <Link href='/contact'>
-                <Button className='py-2 px-4 text-sm sm:py-3 sm:px-6 sm:text-base gap-x-2'>
+            <div className="flex flex-row gap-y-3 gap-x-3 justify-center lg:justify-start mb-12">
+              <Link href="/contact">
+                <Button className="py-2 px-4 text-sm sm:py-3 sm:px-6 sm:text-base gap-x-2">
                   Contact us <Send size={18} />
                 </Button>
               </Link>
@@ -49,14 +45,14 @@ const Hero = () => {
             {/* socials on medium and larger screens */}
           </div>
           {/* image on medium and larger screens */}
-          <div className='hidden md:flex relative'>
+          <div className="hidden lg:flex relative">
             <Image
-              src='/hero/home.JPG'
-              alt='Kamau Wairuri Foundation'
+              src="/hero/home.JPG"
+              alt="Kamau Wairuri Foundation"
               width={550}
               height={500}
-              className='rounded-xl shadow-lg'
-              style={{ width: 'auto', height: 'auto' }}
+              className="rounded-xl shadow-lg"
+              style={{ width: "auto", height: "auto" }}
             />
           </div>
         </div>
